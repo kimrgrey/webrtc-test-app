3cd81e72d960c3f9270fe5742ecbf2bab40ec632
Dmitry Toropchin <dmitry.toropchin@gmail.com>
2016-10-20 20:43:56 +0300

    reworked no-match page

--- app/pages/NoMatch/index.jsx
+++ app/pages/NoMatch/index.jsx
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 
 class NoMatch extends Component {
   render() {
     return (
-      <div>
+      <div className={ classNames('nomatch-container') }>
+        <h1>Such Error...</h1>
+        <h2>So Not Found...</h2>
+        <h3>Wow...</h3>
       </div>
     );
   }

f874966366061f4eafc8df041304e76d1f7cf792
Dmitry Toropchin <dmitry.toropchin@gmail.com>
2016-10-25 00:02:08 +0300

    text chat autoscrolling

--- app/components/Chat/ChatMessage.jsx
+++ app/components/Chat/ChatMessage.jsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 import classNames from 'classnames';
 
 
-const ChatMessage = ({ type, text }) => (
-  <div className={ classNames('chat-message-message', type) }>
-    { text }
-  </div>
-);
+class ChatMessage extends Component {
+  componentDidUpdate() {
+    ReactDOM.findDOMNode(this).scrollIntoView();
+  }
+
+  render() {
+    const { type, text } = this.props;
+
+    return (
+      <div className={ classNames('chat-message-message', type) }>
+        { text }
+      </div>
+    );
+  }
+}
 
 export default ChatMessage;
--- app/components/Chat/ChatMessageArea.jsx
+++ app/components/Chat/ChatMessageArea.jsx
@@ -1,20 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classNames from 'classnames';
 import { map } from 'lodash';
 
 import ChatMessage from './ChatMessage';
 
 
-const ChatMessageArea = ({ messages }) => (
-  <div className={ classNames('chat-message-area') }>
-    <div className={ classNames('chat-message-content') }>
-      {
-        map(messages, (message, index) => (
-          <ChatMessage key={ index } { ...message } />
-        ))
-      }
-    </div>
-  </div>
-);
+class ChatMessageArea extends Component {
+  render() {
+    const { messages } = this.props;
+
+    return (
+      <div className={ classNames('chat-message-area') }>
+        <div className={ classNames('chat-message-content') }>
+          {
+            map(messages, (message, index) => (
+              <ChatMessage key={ index } { ...message } />
+            ))
+          }
+        </div>
+      </div>
+    );
+  }
+}
 
 export default ChatMessageArea;

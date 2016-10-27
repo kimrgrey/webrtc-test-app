b39d612d43ad2788a0958c5e4af3a771ffe63433
Dmitry Toropchin <dmitry.toropchin@gmail.com>
2016-10-27 22:39:29 +0300

    allow joining conference with microphone permission only

--- app/utils/localStream.js
+++ app/utils/localStream.js
@@ -1,7 +1,13 @@
 import config from 'config';
 
 const getLocalStream = () => {
-  return navigator.mediaDevices.getUserMedia(config.mediaConstraints);
+  return navigator.mediaDevices.getUserMedia(config.mediaConstraints)
+    .catch(e => {
+      return navigator.mediaDevices.getUserMedia({
+        ...config.mediaConstraints,
+        video: false,
+      });
+    });
 };
 
 const closeLocalStream = (stream) => {
